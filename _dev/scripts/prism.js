83b3614a4db3e555c6219acbe4280ddaf1624fd0
Warren Shea <warren.shea@gmail.com>
2021-06-28 21:18:36 -0400

    Revise functions to standard

--- _dev/scripts/prism.js
+++ _dev/scripts/prism.js
@@ -1,28 +1,20 @@
 storm_eagle.module('prism', function () {
   'use strict';
 
-  function htmlEncode(str) {
-    var buf = [];
-    for (var i=str.length-1;i>=0;i--) {
-      buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
-    }
-    return buf.join('');
-  }
-
   return {
     initialize: function () {
-      var self = this;
+      const self = this;
       if (document.querySelectorAll(`[data-code-reference]`).length > 0) {
-        self.linkCodeWithPrismSnippet();
+        self.link_code_with_prism_snippet();
       }
     },
-    linkCodeWithPrismSnippet: function () {
+    link_code_with_prism_snippet: function () {
       document.querySelectorAll(`[data-code-reference]`).forEach((el) => {
         var $cleanse = el.cloneNode(true);
         document.querySelector(`code[data-code-snippet=${el.getAttribute("data-code-reference")}`).innerHTML = $cleanse.innerHTML;
       });
     },
-    forcePrismReinit: function () {
+    force_prism_reinit: function () {
       Prism.highlightAll();
     }
   };
