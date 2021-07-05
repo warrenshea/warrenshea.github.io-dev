d484666721d5b18bacbb2576696297d110b8389e
Warren Shea <warren.shea@gmail.com>
2021-07-04 23:40:37 -0400

    Initial Commit - validation for form on page

---
+++ _dev/examples/form-validation/example-form.js
@@ -0,0 +1,31 @@
+storm_eagle.module("example_form", () => {
+  let errormessage = "";
+  return {
+    validate_form: function() {
+      const self = this;
+      errormessage = "";
+
+      if (
+        storm_eagle.form_validation.validate("name", "alpha_numeric") &&
+
+
+      ) {
+      //storm_eagle.form_validation.validate("name2", "alpha_numeric");
+      // storm_eagle.form_validation.validate("province_contact_us", "dropdown");
+      // storm_eagle.form_validation.validate("household_investible_assets_contact_us", "dropdown");
+      // storm_eagle.form_validation.validate("current_invest_contact_us", "dropdown");
+      // storm_eagle.form_validation.validate("what_to_discuss_contact_us", "dropdown");
+      // if (document.querySelector("input[type='radio'][name='contact_preference_contact_us']:checked").value === "phone") {
+      //   storm_eagle.form_validation.validate("phone_number_contact_us", "alpha_numeric");
+      //   storm_eagle.form_validation.validate("best_time_contact_contact_us", "dropdown");
+      // } else if (document.querySelector("input[type='radio'][name='contact_preference_contact_us']:checked").value === "email") {
+      //   storm_eagle.form_validation.validate("email_contact_us", "email");
+      // }
+        return true;
+      } else {
+        return false;
+      }
+
+    }
+  }
+});
