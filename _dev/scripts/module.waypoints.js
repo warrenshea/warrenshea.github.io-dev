db883b0c4aa256b4be39f807dd4c1b136c36cd9c
Warren Shea <warren.shea@gmail.com>
2022-04-10 22:42:09 -0400

    Improved Waypoint module and refactored some code

--- _dev/scripts/module.waypoints.js
+++ _dev/scripts/module.waypoints.js
@@ -9,8 +9,10 @@ storm_eagle.module('waypoint', function () {
       document.querySelectorAll("[data-module='waypoint']").forEach(function(el){
         let waypoint_id = el.getAttribute("id");
         waypoint_state[waypoint_id] = {
+          "element": el.getAttribute("data-waypoint-element"),
           "add_class": el.getAttribute("data-waypoint-add"),
-          "remove_class": el.getAttribute("data-waypoint-remove").split(/[,]+/),
+          "remove_class": el.getAttribute("data-waypoint-remove"),
+          "toggle_class": el.getAttribute("data-waypoint-toggle"),
           "delay": el.getAttribute("data-waypoint-delay"),
           "activate": el.getAttribute("data-waypoint-activate"),
           "new_waypoint_obj": {}
@@ -20,12 +22,19 @@ storm_eagle.module('waypoint', function () {
     },
     add_waypoint_listeners: function (waypoint_id) {
       waypoint_state[waypoint_id]["new_waypoint_obj"] = new Waypoint({
-        element: document.getElementById(waypoint_id),
+        element: ("this") ? document.getElementById(waypoint_id) : document.querySelector(waypoint_state[waypoint_id]["element"]),
         handler: function() {
           let delay = waypoint_state[waypoint_id]["delay"] || 0;
           setTimeout(function(){
-            document.getElementById(waypoint_id).classList.remove(...waypoint_state[waypoint_id]["remove_class"]);
-            document.getElementById(waypoint_id).classList.add(waypoint_state[waypoint_id]["add_class"]);
+            if (waypoint_state[waypoint_id]["remove_class"]) {
+              document.getElementById(waypoint_id).classList.remove(...waypoint_state[waypoint_id]["remove_class"].split(/[,]+/));
+            }
+            if (waypoint_state[waypoint_id]["add_class"]) {
+              document.getElementById(waypoint_id).classList.add(waypoint_state[waypoint_id]["add_class"]);
+            }
+            if (waypoint_state[waypoint_id]["toggle_class"]) {
+              document.getElementById(waypoint_id).classList.toggle(waypoint_state[waypoint_id]["toggle_class"]);
+            }
           }, delay);
         },
         offset: waypoint_state[waypoint_id]["activate"]
