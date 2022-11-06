c74acaad6d0c214a401f5e485207626ff149093b
Warren Shea <warren.shea@gmail.com>
2022-11-06 10:35:03 -0500

    added some fancy animation for the cards

--- _dev/scripts/home.js
+++ _dev/scripts/home.js
@@ -1,6 +1,12 @@
 storm_eagle.module('homepage', () => {
   return {
     initialize: () => {
+      document.querySelectorAll(".opacity\\:0\\%").forEach((el,index) => {
+        setTimeout(() => {
+          el.classList.remove("opacity:0%")
+          el.classList.add("opacity:100%");
+        }, index * 50);
+      });
       document.querySelectorAll(".heading,.description-line,.description-notes-line").forEach((el,index) => {
         setTimeout(() => {
           el.classList.add("opacity:100%");
