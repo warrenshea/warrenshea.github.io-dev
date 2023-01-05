8c2bcc41b98f5f33a7b67d8dbd9853c1313fdf26
Warren Shea <warren.shea@gmail.com>
2023-01-04 20:32:47 -0500

    Add nodejs logo

--- _dev/scripts/home.js
+++ _dev/scripts/home.js
@@ -1,16 +1,17 @@
 storm_eagle.module('homepage', () => {
   return {
     initialize: () => {
-      document.querySelectorAll(".opacity\\:0\\%").forEach((el,index) => {
+      document.querySelectorAll("li.opacity\\:0\\%").forEach((el,index) => {
         setTimeout(() => {
           el.classList.remove("opacity:0%")
           el.classList.add("opacity:100%");
         }, index * 50);
       });
-      document.querySelectorAll(".heading,.description-line,.description-notes-line").forEach((el,index) => {
+      document.querySelectorAll("h2,.heading,.description-line,.description-notes-line").forEach((el,index) => {
         setTimeout(() => {
+          el.classList.remove("opacity:0%")
           el.classList.add("opacity:100%");
-        }, index * 50);
+        }, index * 100);
       });
     }
   }
