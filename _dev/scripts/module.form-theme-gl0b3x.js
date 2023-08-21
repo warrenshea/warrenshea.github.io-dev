bf08e5fc914c9ddccf2afa0f5a66bef762825d1d
Warren Shea <warren.shea@gmail.com>
2023-08-20 22:35:13 -0400

    Color change and some re-working of code

--- _dev/scripts/module.form-theme-gl0b3x.js
+++ _dev/scripts/module.form-theme-gl0b3x.js
@@ -55,7 +55,7 @@ storm_eagle.module('form_parent_checkbox', () => {
     let num_checkboxes = 0;
     for (const [key,value] of Object.entries(parent_checkbox_state[parent_id])) {
       if (num_checkboxes > 0) {
-        if (storm_eagle.form.is_checked(document.getElementById(key))) {
+        if (storm_eagle.checkbox.is_checked(document.getElementById(key))) {
           num_child_checkboxes_checked += 1
         }
       }
@@ -77,25 +77,25 @@ storm_eagle.module('form_parent_checkbox', () => {
   function _update_state(parent_id,id_clicked) {
 
     if (parent_id === id_clicked) {
-      if (storm_eagle.form.is_checked(document.getElementById(parent_id)) === false) {
+      if (storm_eagle.checkbox.is_checked(document.getElementById(parent_id)) === false) {
 
         for (const [key,value] of Object.entries(parent_checkbox_state[parent_id])) {
           parent_checkbox_state[parent_id][key] = false;
-          storm_eagle.form.set_checked(document.getElementById(key),false);
+          storm_eagle.checkbox.set_checked(document.getElementById(key),false);
         }
-      } else if (storm_eagle.form.is_checked(document.getElementById(parent_id)) === true) {
+      } else if (storm_eagle.checkbox.is_checked(document.getElementById(parent_id)) === true) {
         for (const [key,value] of Object.entries(parent_checkbox_state[parent_id])) {
           parent_checkbox_state[parent_id][key] = true;
-          storm_eagle.form.set_checked(document.getElementById(key),true);
+          storm_eagle.checkbox.set_checked(document.getElementById(key),true);
         }
       }
     } else {
-      if (storm_eagle.form.is_checked(document.getElementById(id_clicked)) === true) {
+      if (storm_eagle.checkbox.is_checked(document.getElementById(id_clicked)) === true) {
         parent_checkbox_state[parent_id][id_clicked] = false;
-        storm_eagle.form.set_checked(document.getElementById(id_clicked),false);
-      } else if (storm_eagle.form.is_checked(document.getElementById(id_clicked)) === false) {
+        storm_eagle.checkbox.set_checked(document.getElementById(id_clicked),false);
+      } else if (storm_eagle.checkbox.is_checked(document.getElementById(id_clicked)) === false) {
         parent_checkbox_state[parent_id][id_clicked] = true;
-        storm_eagle.form.set_checked(document.getElementById(id_clicked),true);
+        storm_eagle.checkbox.set_checked(document.getElementById(id_clicked),true);
       }
     }
     _update_parent(parent_id);
@@ -109,12 +109,12 @@ storm_eagle.module('form_parent_checkbox', () => {
         let checkbox_ids = el.getAttribute('aria-controls').split(' ');
 
         parent_checkbox_state[parent_checkbox_id] = {};
-        parent_checkbox_state[parent_checkbox_id][parent_checkbox_id] = storm_eagle.form.is_checked(document.getElementById(parent_checkbox_id));
+        parent_checkbox_state[parent_checkbox_id][parent_checkbox_id] = storm_eagle.checkbox.is_checked(document.getElementById(parent_checkbox_id));
 
         self.add_parent_event_listeners(parent_checkbox_id);
 
         for (let i=0;i<checkbox_ids.length;i++) {
-          parent_checkbox_state[parent_checkbox_id][checkbox_ids[i]] = storm_eagle.form.is_checked(document.getElementById(checkbox_ids[i]));
+          parent_checkbox_state[parent_checkbox_id][checkbox_ids[i]] = storm_eagle.checkbox.is_checked(document.getElementById(checkbox_ids[i]));
           self.add_child_event_listeners(parent_checkbox_id,document.getElementById(checkbox_ids[i]));
         }
       });
