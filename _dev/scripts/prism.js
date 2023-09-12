766bb2e0e8a7bbd1f19730f6b4c4590cdd2c6b22
Warren Shea <warren.shea@gmail.com>
2023-09-12 13:53:40 -0400

    Refactor slightly

--- _dev/scripts/prism.js
+++ _dev/scripts/prism.js
@@ -1,7 +1,15 @@
 storm_eagle.module('prism', () => {
   'use strict';
-
   let self;
+
+  function htmlEncode(str) {
+    var buf = [];
+    for (var i=str.length-1;i>=0;i--) {
+      buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+    }
+    return buf.join('');
+  }
+
   return {
     initialize: () => {
       self = storm_eagle["prism"];
@@ -10,9 +18,8 @@ storm_eagle.module('prism', () => {
       }
     },
     link_code_with_prism_snippet: () => {
-      document.querySelectorAll(`[data-code-reference]`).forEach(el => {
-        let $cleanse = el.cloneNode(true);
-        document.querySelector(`code[data-code-snippet=${el.getAttribute("data-code-reference")}`).innerHTML = $cleanse.innerHTML;
+      document.querySelectorAll(`[data-code-reference]`).forEach((el,index) => {
+        document.querySelector(`code[data-code-snippet=${el.getAttribute("data-code-reference")}`).innerHTML = htmlEncode(el.value);
       });
     },
     force_prism_reinit: () => {
