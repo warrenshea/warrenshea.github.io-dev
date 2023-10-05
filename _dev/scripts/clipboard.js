c1b80e38c524b38660f913a417c98b0e04a62dc1
Warren Shea <warren.shea@gmail.com>
2023-10-04 20:44:17 -0400

    JSPrettier

--- _dev/scripts/clipboard.js
+++ _dev/scripts/clipboard.js
@@ -1,15 +1,16 @@
+'use strict';
 storm_eagle.module('clipboard', () => {
   return {
-    copy: function(text) {
+    copy: function (text) {
       /*https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript*/
       function fallback_copy_text_to_clipboard(text) {
-        var text_area = document.createElement("textarea");
+        var text_area = document.createElement('textarea');
         text_area.value = text;
 
         // Avoid scrolling to bottom
-        text_area.style.top = "0";
-        text_area.style.left = "0";
-        text_area.style.position = "fixed";
+        text_area.style.top = '0';
+        text_area.style.left = '0';
+        text_area.style.position = 'fixed';
 
         document.body.appendChild(textArea);
         text_area.focus();
@@ -29,12 +30,15 @@ storm_eagle.module('clipboard', () => {
         fallback_copy_text_to_clipboard(text);
         return;
       }
-      navigator.clipboard.writeText(text).then(function() {
-        console.log(`Clipboard: ${text}`);
-        //console.log('Async: Copying to clipboard was successful!');
-      }, function(err) {
-        //console.error('Async: Could not copy text: ', err);
-      });
-    }
+      navigator.clipboard.writeText(text).then(
+        () => {
+          console.log(`Clipboard: ${text}`);
+          //console.log('Async: Copying to clipboard was successful!');
+        },
+        function (err) {
+          //console.error('Async: Could not copy text: ', err);
+        },
+      );
+    },
   };
 });
