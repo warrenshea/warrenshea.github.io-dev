ed4e32ba6de504170524f8cad559fa2b1f1d66ec
Warren Shea <warren.shea@gmail.com>
2023-10-10 20:47:40 -0400

    JSPrettier

--- _dev/scripts/home.js
+++ _dev/scripts/home.js
@@ -1,18 +1,19 @@
+'use strict';
 storm_eagle.module('homepage', () => {
   return {
     initialize: () => {
-      document.querySelectorAll("li.opacity\\:0\\%").forEach((el,index) => {
+      document.querySelectorAll('li.opacity\\:0\\%').forEach((el, index) => {
         setTimeout(() => {
-          el.classList.remove("opacity:0%")
-          el.classList.add("opacity:100%");
+          el.classList.remove('opacity:0%');
+          el.classList.add('opacity:100%');
         }, index * 45);
       });
-      document.querySelectorAll("h2,.heading,.description-line,.description-notes-line").forEach((el,index) => {
+      document.querySelectorAll('h2,.heading,.description-line,.description-notes-line').forEach((el, index) => {
         setTimeout(() => {
-          el.classList.remove("opacity:0%")
-          el.classList.add("opacity:100%");
+          el.classList.remove('opacity:0%');
+          el.classList.add('opacity:100%');
         }, index * 25);
       });
-    }
-  }
+    },
+  };
 });
