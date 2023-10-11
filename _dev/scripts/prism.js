06e9c28d75db018c1a73f97df4039498a1d7946c
Warren Shea <warren.shea@gmail.com>
2023-10-10 20:47:21 -0400

    JSPrettier

--- _dev/scripts/prism.js
+++ _dev/scripts/prism.js
@@ -1,10 +1,10 @@
+'use strict';
 storm_eagle.module('prism', () => {
-  'use strict';
   let self;
 
   function html_encode(str) {
     var buf = [];
-    for (var i=str.length-1;i>=0;i--) {
+    for (var i = str.length - 1; i >= 0; i--) {
       buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
     }
     return buf.join('');
@@ -12,18 +12,18 @@ storm_eagle.module('prism', () => {
 
   return {
     initialize: () => {
-      self = storm_eagle["prism"];
-      if (document.querySelectorAll(`[data-code-reference]`).length > 0) {
+      self = storm_eagle['prism'];
+      if (document.querySelectorAll('[data-code-reference]').length > 0) {
         self.link_code_with_prism_snippet();
       }
     },
     link_code_with_prism_snippet: () => {
-      document.querySelectorAll(`[data-code-reference]`).forEach((el,index) => {
-        document.querySelector(`code[data-code-snippet=${el.getAttribute("data-code-reference")}`).innerHTML = html_encode(el.value);
+      document.querySelectorAll('[data-code-reference]').forEach((el, index) => {
+        document.querySelector(`code[data-code-snippet=${el.getAttribute('data-code-reference')}`).innerHTML = html_encode(el.value);
       });
     },
     force_prism_reinit: () => {
       Prism.highlightAll();
-    }
+    },
   };
 });
