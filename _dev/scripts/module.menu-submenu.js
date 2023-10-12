5b9b5f9d900148ecd3266b80327000a2d2bc6dc6
Warren Shea <warren.shea@gmail.com>
2023-10-11 23:39:08 -0400

    JSPrettier + Improved module to allow open on load

--- _dev/scripts/module.menu-submenu.js
+++ _dev/scripts/module.menu-submenu.js
@@ -2,63 +2,69 @@
   @TODO: a11y keyboard functionality
   https://www.w3.org/WAI/ARIA/apg/patterns/menubar/examples/menubar-navigation/
 */
-storm_eagle.module("menu_submenu", () => {
-  "use strict";
+storm_eagle.module('menu_submenu', () => {
+  'use strict';
   let self;
-  let menu_state =  {};
+  let menu_state = {};
   return {
     initialize: () => {
-      self = storm_eagle["menu_submenu"];
-      document.querySelectorAll('[data-module="menu"]').forEach(el => {
-        let menu_id = el.getAttribute("id");
+      self = storm_eagle['menu_submenu'];
+      document.querySelectorAll('[data-module="menu"]').forEach((el) => {
+        let menu_id = el.getAttribute('id');
         menu_state[menu_id] = {
-          "all_triggers": el.querySelectorAll(':scope > li > [data-module="menu.trigger"],:scope > [data-module="menu.trigger"]'),
-          "all_submenus": el.querySelectorAll(':scope > li > [data-module="menu.submenu"],:scope > [data-module="menu.submenu"]'),
-          "active_setting": el.getAttribute('data-menu-active'),
-        }
+          all_triggers: el.querySelectorAll(':scope > li > [data-module="menu.trigger"],:scope > [data-module="menu.trigger"]'),
+          all_submenus: el.querySelectorAll(':scope > li > [data-module="menu.submenu"],:scope > [data-module="menu.submenu"]'),
+          active_setting: el.getAttribute('data-menu-active'),
+          open_on_load: el.querySelectorAll(':scope > li > [data-module="menu.trigger"][data-menu-open],:scope > [data-module="menu.trigger"][data-menu-open]'),
+        };
         self.init_ui(menu_id);
         self.add_event_listeners(menu_id);
+        menu_state[menu_id]['open_on_load'].forEach((el) => {
+          el.click();
+        });
       });
     },
     init_ui: (menu_id) => {
-      menu_state[menu_id]["all_triggers"].forEach((el,index) => {
-        el.setAttribute("tabindex","-1");
-        el.setAttribute("aria-expanded","false");
-        el.setAttribute("aria-controls",el.nextElementSibling.getAttribute("id"));
+      menu_state[menu_id]['all_triggers'].forEach((el, index) => {
+        el.setAttribute('tabindex', '-1');
+        el.setAttribute('aria-expanded', 'false');
+        el.setAttribute('aria-controls', el.nextElementSibling.getAttribute('id'));
         if (index === 0) {
-          el.setAttribute("tabindex","0");
-          el.addEventListener('focusin', () => { document.getElementById(menu_id).classList.add('focus')});
-          el.addEventListener('focusout', () => { document.getElementById(menu_id).classList.remove('focus')});
+          el.setAttribute('tabindex', '0');
+          el.addEventListener('focusin', () => {
+            document.getElementById(menu_id).classList.add('focus');
+          });
+          el.addEventListener('focusout', () => {
+            document.getElementById(menu_id).classList.remove('focus');
+          });
         }
       });
-      menu_state[menu_id]["all_submenus"].forEach((el) => {
-        el.classList.add("display:none");
+      menu_state[menu_id]['all_submenus'].forEach((el) => {
+        el.classList.add('display:none');
       });
-
     },
     add_event_listeners: (menu_id) => {
-      menu_state[menu_id]["all_triggers"].forEach((el) => {
-        el.addEventListener("click", () => {
-          menu_state[menu_id]["all_triggers"].forEach((el) => {
-            el.setAttribute("aria-expanded","false");
+      menu_state[menu_id]['all_triggers'].forEach((el) => {
+        el.addEventListener('click', () => {
+          menu_state[menu_id]['all_triggers'].forEach((el) => {
+            el.setAttribute('aria-expanded', 'false');
           });
-          el.setAttribute("aria-expanded","true");
-          self.open(menu_id,el.getAttribute("aria-controls"));
+          el.setAttribute('aria-expanded', 'true');
+          self.open(menu_id, el.getAttribute('aria-controls'));
         });
       });
     },
-    open: (menu_id,id) => {
-      if (menu_state[menu_id]["active_setting"] === "single") {
-        menu_state[menu_id]["all_submenus"].forEach((el) => {
-          el.classList.add("display:none");
+    open: (menu_id, id) => {
+      if (menu_state[menu_id]['active_setting'] === 'single') {
+        menu_state[menu_id]['all_submenus'].forEach((el) => {
+          el.classList.add('display:none');
         });
-        document.getElementById(id).classList.remove("display:none");
+        document.getElementById(id).classList.remove('display:none');
         storm_eagle.equalize_heights.force_resize();
-      } else if (menu_state[menu_id]["active_setting"] === "multiple") {
-        document.getElementById(id).classList.toggle("display:none");
+      } else if (menu_state[menu_id]['active_setting'] === 'multiple') {
+        document.getElementById(id).classList.toggle('display:none');
         storm_eagle.equalize_heights.force_resize();
       }
-
     },
   };
 });
