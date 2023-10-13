6b8f749f264344486d107fd06532a81760ea3d67
Warren Shea <warren.shea@gmail.com>
2023-10-12 22:00:13 -0400

    Improvement on classes

--- _dev/scripts/module.menu-submenu.js
+++ _dev/scripts/module.menu-submenu.js
@@ -15,6 +15,8 @@ storm_eagle.module('menu_submenu', () => {
           all_triggers: el.querySelectorAll(':scope > li > [data-module="menu.trigger"],:scope > [data-module="menu.trigger"]'),
           all_submenus: el.querySelectorAll(':scope > li > [data-module="menu.submenu"],:scope > [data-module="menu.submenu"]'),
           active_setting: el.getAttribute('data-menu-active'),
+          active_classes: el.getAttribute('data-menu-active-classes') ? el.getAttribute('data-menu-active-classes').split(',') : '',
+          inactive_classes: el.getAttribute('data-menu-inactive-classes') ? el.getAttribute('data-menu-inactive-classes').split(',') : '',
           open_on_load: el.querySelectorAll(':scope > li > [data-module="menu.trigger"][data-menu-open],:scope > [data-module="menu.trigger"][data-menu-open]'),
         };
         self.init_ui(menu_id);
@@ -29,6 +31,7 @@ storm_eagle.module('menu_submenu', () => {
         el.setAttribute('tabindex', '-1');
         el.setAttribute('aria-expanded', 'false');
         el.setAttribute('aria-controls', el.nextElementSibling.getAttribute('id'));
+        el.classList.add(...menu_state[menu_id]['inactive_classes']);
         if (index === 0) {
           el.setAttribute('tabindex', '0');
           el.addEventListener('focusin', () => {
@@ -46,10 +49,27 @@ storm_eagle.module('menu_submenu', () => {
     add_event_listeners: (menu_id) => {
       menu_state[menu_id]['all_triggers'].forEach((el) => {
         el.addEventListener('click', () => {
-          menu_state[menu_id]['all_triggers'].forEach((el) => {
-            el.setAttribute('aria-expanded', 'false');
-          });
-          el.setAttribute('aria-expanded', 'true');
+          let aria_expanded_initial_state = el.getAttribute('aria-expanded');
+          if (menu_state[menu_id]['active_setting'] === 'single') {
+            menu_state[menu_id]['all_triggers'].forEach((el) => {
+              el.classList.remove(...menu_state[menu_id]['active_classes']);
+              el.classList.add(...menu_state[menu_id]['inactive_classes']);
+              el.setAttribute('aria-expanded', 'false');
+            });
+            el.setAttribute('aria-expanded', 'true');
+            el.classList.add(...menu_state[menu_id]['active_classes']);
+            el.classList.remove(...menu_state[menu_id]['inactive_classes']);
+          } else if (menu_state[menu_id]['active_setting'] === 'multiple') {
+            if (aria_expanded_initial_state === 'false') {
+              el.setAttribute('aria-expanded', 'true');
+              el.classList.add(...menu_state[menu_id]['active_classes']);
+              el.classList.remove(...menu_state[menu_id]['inactive_classes']);
+            } else {
+              el.setAttribute('aria-expanded', 'false');
+              el.classList.remove(...menu_state[menu_id]['active_classes']);
+              el.classList.add(...menu_state[menu_id]['inactive_classes']);
+            }
+          }
           self.open(menu_id, el.getAttribute('aria-controls'));
         });
       });
