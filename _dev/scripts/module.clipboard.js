1390fb26baa81bf0ffe205944e5093fe880c564a
Warren Shea <warren.shea@gmail.com>
2023-11-16 07:54:49 -0500

    Arrow function + minor refactor

--- _dev/scripts/module.clipboard.js
+++ _dev/scripts/module.clipboard.js
@@ -1,9 +1,9 @@
 'use strict';
 storm_eagle.module('clipboard', () => {
   return {
-    copy: function (text,text_color,background) {
+    copy: (text,text_color,background) => {
       /*https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript*/
-      function fallback_copy_text_to_clipboard(text) {
+      const fallback_copy_text_to_clipboard = (text) => {
         var text_area = document.createElement('textarea');
         text_area.value = text;
 
@@ -30,18 +30,18 @@ storm_eagle.module('clipboard', () => {
         fallback_copy_text_to_clipboard(text);
         return;
       }
-      navigator.clipboard.writeText(text).then(
-        () => {
+      navigator.clipboard.writeText(text)
+        .then(() => {
           console.log(`Clipboard: ${text}`);
+
           if (storm_eagle.toast) {
-            storm_eagle.toast.add(`Copied: ${text}`,text_color, background);
+            storm_eagle.toast.add(`Copied: ${text}`, text_color, background);
           }
           //console.log('Async: Copying to clipboard was successful!');
-        },
-        function (err) {
+        })
+        .catch((err) => {
           //console.error('Async: Could not copy text: ', err);
-        },
-      );
+        });
     },
   };
 });
