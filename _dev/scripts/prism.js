aa275bc534bbf1807aa0789e992b6241348b14f2
Warren Shea <warren.shea@gmail.com>
2023-11-16 07:53:40 -0500

    Arrow function

--- _dev/scripts/prism.js
+++ _dev/scripts/prism.js
@@ -2,7 +2,7 @@
 storm_eagle.module('prism', () => {
   let self;
 
-  function html_encode(str) {
+  const html_encode = (str) => {
     var buf = [];
     for (var i = str.length - 1; i >= 0; i--) {
       buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
