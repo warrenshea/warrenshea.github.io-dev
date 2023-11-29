2ff1a1cf2bbc30318b19721f54af57b5e836578e
Warren Shea <warren.shea@gmail.com>
2023-11-28 23:23:50 -0500

    Refactor to handle event listeners and simply state code

--- _dev/scripts/module.form.switch.button.js
+++ _dev/scripts/module.form.switch.button.js
@@ -1,13 +1,38 @@
 'use strict';
 storm_eagle.module('form_switch_button', () => {
   let self;
-  let switch_button_state = {};
+  let state = {};
+
+  const handle_click = (event) => {
+    let el_state = state[event.currentTarget.getAttribute("id")];
+    let switch_active_state = event.currentTarget.getAttribute("aria-checked") === "true";
+    if (switch_active_state) {
+      el_state['slider'].setAttribute("data-switch",'');
+      el_state['slider'].classList.remove(...el_state['bg_active_classes']);
+      el_state['slider'].classList.add(...el_state['bg_inactive_classes']);
+      el_state['thumb'].classList.remove(...el_state['thumb_active_class']);
+      el_state['thumb'].classList.add(...el_state['thumb_inactive_class']);
+      el_state['text'].classList.remove(...el_state['text_active_classes']);
+      el_state['text'].classList.add(...el_state['text_inactive_classes']);
+    } else {
+      el_state['slider'].setAttribute("data-switch",'active');
+      el_state['slider'].classList.remove(...el_state['bg_inactive_classes']);
+      el_state['slider'].classList.add(...el_state['bg_active_classes']);
+      el_state['thumb'].classList.remove(...el_state['thumb_inactive_class']);
+      el_state['thumb'].classList.add(...el_state['thumb_active_class']);
+      el_state['text'].classList.remove(...el_state['text_inactive_classes']);
+      el_state['text'].classList.add(...el_state['text_active_classes']);
+    }
+    event.currentTarget.setAttribute("aria-checked", (!switch_active_state).toString());
+  };
+
   return {
     initialize: () => {
       self = storm_eagle['form_switch_button'];
+      state = {};
       document.querySelectorAll('[data-module="switch.button"]').forEach((el) => {
-        let switch_button_id = el.getAttribute('id');
-        switch_button_state[switch_button_id] = {
+        let id = el.getAttribute('id');
+        state[id] = {
           slider: el.querySelector(':scope > [data-module="switch.slider"]'),
           thumb: el.querySelector(':scope > [data-module="switch.slider"] > [data-module="switch.thumb"]'),
           text: el.querySelector(':scope > [data-module="switch.slider"] > [data-module="switch.text"]'),
@@ -19,39 +44,16 @@ storm_eagle.module('form_switch_button', () => {
           thumb_active_class: el.getAttribute('data-switch-thumb-active-class') ? el.getAttribute('data-switch-thumb-active-class').split(',') : '',
           thumb_inactive_class: el.getAttribute('data-switch-thumb-inactive-class') ? el.getAttribute('data-switch-thumb-inactive-class').split(',') : '',
         }
-        self.init_ui(switch_button_id);
-        self.button_switch_listener(switch_button_id);
+        self.init_ui(id);
+        document.querySelector(`#${id}`).removeEventListener("click",handle_click);
+        document.querySelector(`#${id}`).addEventListener("click", handle_click);
       });
-      console.log(switch_button_state);
     },
-    init_ui: (switch_button_id) => {
-      switch_button_state[switch_button_id]["label"].setAttribute("for",switch_button_id);
-      switch_button_state[switch_button_id]['slider'].classList.add(...switch_button_state[switch_button_id]['bg_inactive_classes']);
-      switch_button_state[switch_button_id]['thumb'].classList.add(...switch_button_state[switch_button_id]['thumb_inactive_class']);
-      switch_button_state[switch_button_id]['text'].classList.add(...switch_button_state[switch_button_id]['text_inactive_classes']);
-    },
-    button_switch_listener: (switch_button_id) => {
-      document.querySelector(`#${switch_button_id}`).addEventListener("click", (event) => {
-        let switch_active_state = event.currentTarget.getAttribute("aria-checked") === "true";
-        if (switch_active_state) {
-          switch_button_state[switch_button_id]['slider'].setAttribute("data-switch",'');
-          switch_button_state[switch_button_id]['slider'].classList.remove(...switch_button_state[switch_button_id]['bg_active_classes']);
-          switch_button_state[switch_button_id]['slider'].classList.add(...switch_button_state[switch_button_id]['bg_inactive_classes']);
-          switch_button_state[switch_button_id]['thumb'].classList.remove(...switch_button_state[switch_button_id]['thumb_active_class']);
-          switch_button_state[switch_button_id]['thumb'].classList.add(...switch_button_state[switch_button_id]['thumb_inactive_class']);
-          switch_button_state[switch_button_id]['text'].classList.remove(...switch_button_state[switch_button_id]['text_active_classes']);
-          switch_button_state[switch_button_id]['text'].classList.add(...switch_button_state[switch_button_id]['text_inactive_classes']);
-        } else {
-          switch_button_state[switch_button_id]['slider'].setAttribute("data-switch",'active');
-          switch_button_state[switch_button_id]['slider'].classList.remove(...switch_button_state[switch_button_id]['bg_inactive_classes']);
-          switch_button_state[switch_button_id]['slider'].classList.add(...switch_button_state[switch_button_id]['bg_active_classes']);
-          switch_button_state[switch_button_id]['thumb'].classList.remove(...switch_button_state[switch_button_id]['thumb_inactive_class']);
-          switch_button_state[switch_button_id]['thumb'].classList.add(...switch_button_state[switch_button_id]['thumb_active_class']);
-          switch_button_state[switch_button_id]['text'].classList.remove(...switch_button_state[switch_button_id]['text_inactive_classes']);
-          switch_button_state[switch_button_id]['text'].classList.add(...switch_button_state[switch_button_id]['text_active_classes']);
-        }
-        event.currentTarget.setAttribute("aria-checked", (!switch_active_state).toString());
-      });
+    init_ui: (id) => {
+      state[id]["label"].setAttribute("for",id);
+      state[id]['slider'].classList.add(...state[id]['bg_inactive_classes']);
+      state[id]['thumb'].classList.add(...state[id]['thumb_inactive_class']);
+      state[id]['text'].classList.add(...state[id]['text_inactive_classes']);
     },
   };
 });
