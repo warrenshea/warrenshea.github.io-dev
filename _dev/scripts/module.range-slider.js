cf11aca04b2140e5d3ccca5b9f96f36be7c730ef
Warren Shea <warren.shea@gmail.com>
2023-12-04 00:25:58 -0500

    Restored label resize listener code (oops!)

--- _dev/scripts/module.range-slider.js
+++ _dev/scripts/module.range-slider.js
@@ -16,9 +16,9 @@ storm_eagle.module('range_slider', () => {
       self = storm_eagle.range_slider;
       module_state = {};
       document.querySelectorAll('[data-module="range-slider.input-container"]').forEach((container) => {
-        const el1 = container.querySelector(':scope > [data-module="range-slider.input-1"]');
+        const el1 = container.querySelector('[data-module="range-slider.input-1"]');
         const id1 = el1.getAttribute('id');
-        const el2 = container.querySelector(':scope > [data-module="range-slider.input-2"]');
+        const el2 = container.querySelector('[data-module="range-slider.input-2"]');
         const id2 = el2.getAttribute('id');
         module_state[id1] = {
           id1,
@@ -32,6 +32,12 @@ storm_eagle.module('range_slider', () => {
         };
         self.manage_slider_input_listeners(id1);
         self.update_slider_track(id1);
+        self.resize_listener(id1);
+      });
+    },
+    ready: () => {
+      document.querySelectorAll('[data-module="range-slider.input-1"]').forEach((el) => {
+        self.label_resize(el.getAttribute('id'));
       });
     },
     manage_slider_input_listeners: (id1) => {
@@ -54,6 +60,22 @@ storm_eagle.module('range_slider', () => {
       slider_fill.style.left = `${left}%`;
       const percentage = ((el2.value - el2.getAttribute('min')) / (el2.getAttribute('max') - el2.getAttribute('min'))) * 100;
       slider_fill.style.width = `${percentage - left}%`;
+    },
+    label_resize: (id1) => {
+      const { slider_container, labels, label_container } = module_state[id1];
+      const num_labels = labels.length;
+      let container_width = slider_container.offsetWidth;
+      let full_label_width = (container_width * num_labels) / (num_labels - 1);
+      label_container.style.width = `${full_label_width}px`;
+      let transform_left = (50 / num_labels) * -1;
+      label_container.style.transform = `translateX(${transform_left}%)`;
+    },
+    resize_listener: (id1) => {
+      const { el1 } = module_state[id1];
+      const label_resize = () => {
+        self.label_resize(id1);
+      }
+      storm_eagle.resize_observer(el1, label_resize);
     },
   };
 });
