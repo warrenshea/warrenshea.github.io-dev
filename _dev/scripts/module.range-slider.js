0c39ef5b779ce66ebd1dca2e28b0173d6430a1cf
Warren Shea <warren.shea@gmail.com>
2023-12-18 21:13:51 -0500

    Refactor for ui section

--- _dev/scripts/module.range-slider.js
+++ _dev/scripts/module.range-slider.js
@@ -22,14 +22,37 @@ storm_eagle.module('range_slider', () => {
           labels: container.querySelectorAll(':scope > [data-module="slider.labels"] > * '),
           slider_fill: container.querySelector(':scope > [data-module="slider.background"] > [data-module="slider.fill"]'),
         };
+        self.ui.initialize(id1);
         self.event_listeners.initialize(id1);
-        self.update_slider_track(id1);
       });
     },
-    ready: () => {
-      document.querySelectorAll('[data-module="range-slider.input-1"]').forEach((el) => {
-        self.label_resize(el.getAttribute('id'));
-      });
+    ui: {
+      initialize: (id1) => {
+        self.ui.label_resize(id1);
+        self.ui.update_slider_track(id1);
+      },
+      label_resize: (id1) => {
+        const { slider_container, labels, label_container } = module_state[id1];
+        const num_labels = labels.length;
+        const container_width = slider_container.offsetWidth;
+        const full_label_width = (container_width * num_labels) / (num_labels - 1);
+        label_container.style.width = `${full_label_width}px`;
+        const transform_left = (50 / num_labels) * -1;
+        label_container.style.transform = `translateX(${transform_left}%)`;
+      },
+      update_slider_track: (id1) => {
+        const { slider_fill } = module_state[id1];
+        let { el1, el2 } = module_state[id1];
+        if (parseInt(el1.value) > parseInt(el2.value)) {
+          const temp = el1;
+          el1 = el2;
+          el2 = temp;
+        }
+        const left = ((el1.value - el1.getAttribute('min')) / (el1.getAttribute('max') - el1.getAttribute('min'))) * 100;
+        slider_fill.style.left = `${left}%`;
+        const percentage = ((el2.value - el2.getAttribute('min')) / (el2.getAttribute('max') - el2.getAttribute('min'))) * 100;
+        slider_fill.style.width = `${percentage - left}%`;
+      },
     },
     event_listeners: {
       initialize: (id1) => {
@@ -41,40 +64,19 @@ storm_eagle.module('range_slider', () => {
         self.event_listeners.resize_listener(id1);
       },
       handle_slider_change_1: (event) => {
-        self.update_slider_track(event.currentTarget.getAttribute('id'));
+        self.ui.update_slider_track(event.currentTarget.getAttribute('id'));
       },
       handle_slider_change_2: (event) => {
-        self.update_slider_track(event.currentTarget.previousElementSibling.getAttribute('id'));
+        self.ui.update_slider_track(event.currentTarget.previousElementSibling.getAttribute('id'));
       },
       resize_listener: (id1) => {
         const { el1 } = module_state[id1];
         const label_resize = () => {
-          self.label_resize(id1);
+          self.ui.label_resize(id1);
         };
         storm_eagle.resize_observer(el1, label_resize);
       },
     },
-    update_slider_track: (id1) => {
-      const { slider_fill } = module_state[id1];
-      let { el1, el2 } = module_state[id1];
-      if (parseInt(el1.value) > parseInt(el2.value)) {
-        const temp = el1;
-        el1 = el2;
-        el2 = temp;
-      }
-      const left = ((el1.value - el1.getAttribute('min')) / (el1.getAttribute('max') - el1.getAttribute('min'))) * 100;
-      slider_fill.style.left = `${left}%`;
-      const percentage = ((el2.value - el2.getAttribute('min')) / (el2.getAttribute('max') - el2.getAttribute('min'))) * 100;
-      slider_fill.style.width = `${percentage - left}%`;
-    },
-    label_resize: (id1) => {
-      const { slider_container, labels, label_container } = module_state[id1];
-      const num_labels = labels.length;
-      const container_width = slider_container.offsetWidth;
-      const full_label_width = (container_width * num_labels) / (num_labels - 1);
-      label_container.style.width = `${full_label_width}px`;
-      const transform_left = (50 / num_labels) * -1;
-      label_container.style.transform = `translateX(${transform_left}%)`;
-    },
+
   };
 });
