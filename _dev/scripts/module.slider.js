21e0b044c4d46b13d801e5b328c87b097c8bb446
Warren Shea <warren.shea@gmail.com>
2023-12-18 21:13:54 -0500

    Refactor for ui section

--- _dev/scripts/module.slider.js
+++ _dev/scripts/module.slider.js
@@ -18,14 +18,29 @@ storm_eagle.module('slider', () => {
           labels: container.querySelectorAll(':scope > [data-module="slider.labels"] > * '),
           slider_fill: container.querySelector(':scope > [data-module="slider.background"] > [data-module="slider.fill"]'),
         };
+        self.ui.initialize(id);
         self.event_listeners.initialize(id);
-        self.update_slider_track(id);
       });
     },
-    ready: () => {
-      document.querySelectorAll("[data-module='slider.input']").forEach((el) => {
-        self.label_resize(el.getAttribute('id'));
-      });
+    ui: {
+      initialize: (id) => {
+        self.ui.label_resize(id);
+        self.ui.update_slider_track(id);
+      },
+      label_resize: (id) => {
+        const { slider_container, labels, label_container } = module_state[id];
+        const num_labels = labels.length;
+        const container_width = slider_container.offsetWidth;
+        const full_label_width = (container_width * num_labels) / (num_labels - 1);
+        label_container.style.width = `${full_label_width}px`;
+        const transform_left = (50 / num_labels) * -1;
+        label_container.style.transform = `translateX(${transform_left}%)`;
+      },
+      update_slider_track: (id) => {
+        const { el, slider_fill } = module_state[id];
+        const percentage = Math.round(((el.value - el.getAttribute('min')) / (el.getAttribute('max') - el.getAttribute('min'))) * 100);
+        slider_fill.style.width = `${percentage}%`;
+      },
     },
     event_listeners: {
       initialize: (id) => {
@@ -35,29 +50,15 @@ storm_eagle.module('slider', () => {
         self.event_listeners.resize_listener(id);
       },
       handle_slider_change: (event) => {
-        self.update_slider_track(event.currentTarget.getAttribute('id'));
+        self.ui.update_slider_track(event.currentTarget.getAttribute('id'));
       },
       resize_listener: (id) => {
         const { el } = module_state[id];
         const label_resize = () => {
-          self.label_resize(id);
+          self.ui.label_resize(id);
         };
         storm_eagle.resize_observer(el, label_resize);
       },
-    },
-    update_slider_track: (id) => {
-      const { el, slider_fill } = module_state[id];
-      const percentage = Math.round(((el.value - el.getAttribute('min')) / (el.getAttribute('max') - el.getAttribute('min'))) * 100);
-      slider_fill.style.width = `${percentage}%`;
-    },
-    label_resize: (id) => {
-      const { slider_container, labels, label_container } = module_state[id];
-      const num_labels = labels.length;
-      const container_width = slider_container.offsetWidth;
-      const full_label_width = (container_width * num_labels) / (num_labels - 1);
-      label_container.style.width = `${full_label_width}px`;
-      const transform_left = (50 / num_labels) * -1;
-      label_container.style.transform = `translateX(${transform_left}%)`;
     },
   };
 });
