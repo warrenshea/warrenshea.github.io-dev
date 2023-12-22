19b81070d535d496f8cd332e154c8a11eea107c8
Warren Shea <warren.shea@gmail.com>
2023-12-22 00:13:47 -0500

    Standardize code + minor refactor

--- _dev/scripts/module.draggable.js
+++ _dev/scripts/module.draggable.js
@@ -1,90 +1,105 @@
 'use strict';
 
-var draggable_storage = {
-  destination_type: "",
-  code: "",
-  callback: ""
-};
-
 storm_eagle.module('draggable', () => {
   let self;
-  let draggable_state = {};
+  let module_state = {};
   return {
+    data: {
+      destination_type: "",
+      code: "",
+      callback: ""
+    },
     initialize: () => {
       self = storm_eagle.draggable;
+      module_state = {};
       document.querySelectorAll('[data-module="module.draggable"] [data-module="draggable.trigger"]').forEach((el) => {
-        let draggable_id = el.getAttribute('id');
-        draggable_state[draggable_id] = {
+        const id = el.getAttribute('id');
+        module_state[id] = {
+          el,
           source: el.getAttribute('data-draggable-source'),
           type: el.getAttribute('data-draggable-type'),
           destination_type: el.getAttribute('data-draggable-destination-type'),
           callback: el.getAttribute('data-draggable-callback') || "",
         };
-        self.add_trigger_event_listeners(draggable_id);
+        self.event_listeners.trigger.initialize(id);
       });
       document.querySelectorAll('[data-module="module.draggable"] [data-module="draggable.droparea"]').forEach((el) => {
-        self.add_destination_event_listeners(el);
+        self.event_listeners.destination.initialize(el);
       });
     },
-    add_trigger_event_listeners: (draggable_id) => {
+    event_listeners: {
+      trigger: {
+        initialize: (id) => {
+          const { el } = module_state[id];
+          el.removeEventListener('mouseover', self.event_listeners.trigger.handle_mouseover);
+          el.addEventListener('mouseover', self.event_listeners.trigger.handle_mouseover);
+          el.removeEventListener('dragstart', self.event_listeners.trigger.handle_dragstart);
+          el.addEventListener('dragstart', self.event_listeners.trigger.handle_dragstart);
+        },
+        handle_mouseover: async (event) => {
+          const id = event.currentTarget.getAttribute('id');
+          const { source, type, destination_type, callback } = module_state[id];
+          self.data.destination_type = destination_type;
+          self.data.callback = callback;
 
-      document.getElementById(draggable_id).addEventListener('mouseover', async (event) => {
-        draggable_storage.destination_type = draggable_state[draggable_id]["destination_type"];
-        draggable_storage.callback = draggable_state[draggable_id]["callback"];
-
-        switch (draggable_state[draggable_id]["type"]) {
-          case "async_function":
-            //console.log("source type: async function");
-            draggable_storage.code = await storm_eagle.util.run_str_func(draggable_state[draggable_id]["source"]);
-            break;
-          case "function":
-            //console.log("source type: function");
-            storm_eagle.util.run_str_func(draggable_state[draggable_id]["source"])
-              .then((result) => {
-                draggable_storage.code = result;
-              })
-              .catch((error) => {
-                console.error('Promise rejected:', error);
-              });
-            break;
-          case "query_selector":
-            //console.log("source type: query selector");
-            draggable_storage.code = document.querySelector(draggable_state[draggable_id]["source"]).innerHTML;
-            break;
-          case "string":
-            //console.log("source type: string");
-            draggable_storage.code = draggable_state[draggable_id]["source"];
-            break;
-          default:
-        }
-      });
-
-      document.getElementById(draggable_id).addEventListener('dragstart', (event) => {
-        event.dataTransfer.setData('text/plain', JSON.stringify(draggable_storage));
-      });
-    },
-    add_destination_event_listeners: (el) => {
-
-      el.addEventListener('dragover', (event) => {
-        event.preventDefault();
-      });
-
-      el.addEventListener('dragenter', (event) => {
-        event.preventDefault();
-      });
-
-      el.addEventListener('drop', (event) => {
-        event.preventDefault();
-        const draggable_storage_temp = JSON.parse(event.dataTransfer.getData('text/plain'));
-        if (draggable_storage_temp.destination_type === "child") {
-          el.innerHTML = draggable_storage_temp.code;
-        } else if (draggable_storage_temp.destination_type === "replace") {
-          el.outerHTML = draggable_storage_temp.code;
-        } else if (draggable_storage_temp.destination_type === "above") {
-          el.insertAdjacentHTML('beforebegin', draggable_storage_temp.code);
-        }
-      });
+          switch (type) {
+            case "async_function":
+              //console.log("source type: async function");
+              self.data.code = await storm_eagle.util.run_str_func(source);
+              break;
+            case "function":
+              //console.log("source type: function");
+              storm_eagle.util.run_str_func(source)
+                .then((result) => {
+                  self.data.code = result;
+                })
+                .catch((error) => {
+                  console.error('Promise rejected:', error);
+                });
+              break;
+            case "query_selector":
+              //console.log("source type: query selector");
+              self.data.code = document.querySelector(source).innerHTML;
+              break;
+            case "string":
+              //console.log("source type: string");
+              self.data.code = source;
+              break;
+            default:
+          }
+        },
+        handle_dragstart: (event) => {
+          event.dataTransfer.setData('text/plain', JSON.stringify(self.data));
+        },
+      },
+      destination: {
+        initialize: (el) => {
+          el.removeEventListener('dragover', self.event_listeners.destination.handle_dragover);
+          el.addEventListener('dragover', self.event_listeners.destination.handle_dragover);
+          el.removeEventListener('dragenter', self.event_listeners.destination.handle_dragenter);
+          el.addEventListener('dragenter', self.event_listeners.destination.handle_dragenter);
+          el.removeEventListener('drop', self.event_listeners.destination.handle_drop);
+          el.addEventListener('drop', self.event_listeners.destination.handle_drop);
+        },
+        handle_dragover: (event) => {
+          event.preventDefault();
+        },
+        handle_dragenter: (event) => {
+          event.preventDefault();
+        },
+        handle_drop: (event) => {
+          const el = event.currentTarget;
+          event.preventDefault();
+          const data_temp = JSON.parse(event.dataTransfer.getData('text/plain'));
+          if (data_temp.destination_type === "child") {
+            el.innerHTML = data_temp.code;
+          } else if (data_temp.destination_type === "replace") {
+            el.outerHTML = data_temp.code;
+          } else if (data_temp.destination_type === "above") {
+            el.insertAdjacentHTML('beforebegin', data_temp.code);
+          }
+        },
+      },
     }
-
   };
 });
