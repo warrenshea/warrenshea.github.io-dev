0aaef4e2c20bdbef41fef432e22d1a1ea84406b8
Warren Shea <warren.shea@gmail.com>
2023-12-22 00:15:46 -0500

    Minor refactor

--- _dev/scripts/module.waypoint.js
+++ _dev/scripts/module.waypoint.js
@@ -1,44 +1,42 @@
 'use strict';
 storm_eagle.module('waypoint', () => {
   let self;
-  let waypoint_state = {};
+  let module_state = {};
 
   return {
     initialize: () => {
       self = storm_eagle.waypoint;
+      module_state = {};
       document.querySelectorAll("[data-module='waypoint']").forEach((el) => {
-        let waypoint_id = el.getAttribute('id');
-        waypoint_state[waypoint_id] = {
+        const id = el.getAttribute('id');
+        module_state[id] = {
+          el,
           element: el.getAttribute('data-waypoint-element'),
-          add_class: el.getAttribute('data-waypoint-add'),
-          remove_class: el.getAttribute('data-waypoint-remove'),
-          toggle_class: el.getAttribute('data-waypoint-toggle'),
+          add_class: el.getAttribute('data-waypoint-add-classes') ? el.getAttribute('data-waypoint-add-classes').split(',') : '',
+          remove_class: el.getAttribute('data-waypoint-remove-classes') ? el.getAttribute('data-waypoint-remove-classes').split(',') : '',
+          toggle_class: el.getAttribute('data-waypoint-toggle-classes') ? el.getAttribute('data-waypoint-toggle-classes').split(',') : '',
           delay: el.getAttribute('data-waypoint-delay'),
           activate: el.getAttribute('data-waypoint-activate'),
           new_waypoint_obj: {},
         };
-        self.add_waypoint_listeners(waypoint_id);
+        self.event_listeners.initialize(id);
       });
     },
-    add_waypoint_listeners: (waypoint_id) => {
-      waypoint_state[waypoint_id]['new_waypoint_obj'] = new Waypoint({
-        element: 'this' ? document.getElementById(waypoint_id) : document.querySelector(waypoint_state[waypoint_id]['element']),
-        handler: () => {
-          let delay = waypoint_state[waypoint_id]['delay'] || 0;
-          setTimeout(() => {
-            if (waypoint_state[waypoint_id]['remove_class']) {
-              document.getElementById(waypoint_id).classList.remove(...waypoint_state[waypoint_id]['remove_class'].split(/[,]+/));
-            }
-            if (waypoint_state[waypoint_id]['add_class']) {
-              document.getElementById(waypoint_id).classList.add(waypoint_state[waypoint_id]['add_class']);
-            }
-            if (waypoint_state[waypoint_id]['toggle_class']) {
-              document.getElementById(waypoint_id).classList.toggle(waypoint_state[waypoint_id]['toggle_class']);
-            }
-          }, delay);
-        },
-        offset: waypoint_state[waypoint_id]['activate'],
-      });
+    event_listeners: {
+      initialize: (id) => {
+        const { element, add_class, remove_class, toggle_class, delay=0, activate } = module_state[id];
+        module_state[id]['new_waypoint_obj'] = new Waypoint({
+          element: 'this' ? document.getElementById(id) : document.querySelector(element),
+          handler: () => {
+            setTimeout(() => {
+              (remove_class) && document.getElementById(id).classList.remove(...remove_class);
+              (add_class) && document.getElementById(id).classList.add(...add_class);
+              (toggle_class) && document.getElementById(id).classList.toggle(...toggle_class);
+            }, delay);
+          },
+          offset: activate,
+        });
+      }
     },
   };
 });
