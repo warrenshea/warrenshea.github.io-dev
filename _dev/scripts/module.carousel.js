db63fa8c66e5839ee02444d361fc87219ab5ff76
Warren Shea <warren.shea@gmail.com>
2023-12-22 00:13:21 -0500

    Majot refactor

--- _dev/scripts/module.carousel.js
+++ _dev/scripts/module.carousel.js
@@ -10,13 +10,12 @@ storm_eagle.module('carousel', () => {
       self = storm_eagle.carousel;
       module_state = {};
       document.querySelectorAll('[data-module="carousel"]').forEach((el) => {
-        let id = el.getAttribute('id');
+        const id = el.getAttribute('id');
         module_state[id] = {
-          id,
           el,
           item_group: el.querySelector('[data-module="carousel.item-group"]'),
           items: el.querySelectorAll('[data-module="carousel.item"]:not(.display\\:none)'),
-          total_children: el.querySelectorAll('[data-module="carousel.item"]:not(.display\\:none)').length,
+          item_width: '',
           indicators_group: el.querySelector('[data-module="carousel.indicators-group"]'),
           indicators_group_controls: el.querySelectorAll('[data-module="carousel.indicators-group"] > [data-carousel-indicator-control]'),
           controls: el.querySelectorAll('[data-module="carousel.controls"]'),
@@ -32,7 +31,6 @@ storm_eagle.module('carousel', () => {
           number_of_active: '',
           offset_left_array: JSON.parse(el.getAttribute('data-carousel-offset')),
           offset_left: '',
-          carousel_item_width: '',
           current_active_carousel_item: parseInt(el.getAttribute('data-carousel-item-active')) || 0,
         };
         self.ui.update(id);
@@ -40,13 +38,27 @@ storm_eagle.module('carousel', () => {
       });
     },
     state: {
-      initialize: (id) => {
+      update: (id) => {
         //console.log("initialize");
+        self.state.reinitiailize(id);
         self.state.set_offset_left(id);
         self.state.set_number_of_active(id);
         self.state.set_transition_duration(id);
         self.state.set_indicators(id);
-        self.state.set_carousel_item_width(id);
+        self.state.set_item_width(id);
+      },
+      reinitiailize: (id) => {
+        //console.log("set_items");
+        const { el } = module_state[id];
+        module_state[id]['items'] = el.querySelectorAll('[data-module="carousel.item"]:not(.display\\:none)');
+        module_state[id]['transition_duration_array'] = JSON.parse(el.getAttribute('data-carousel-transition-duration'));
+        module_state[id]['number_of_active_array'] = JSON.parse(el.getAttribute('data-carousel-number-active'));
+        module_state[id]['offset_left_array'] = JSON.parse(el.getAttribute('data-carousel-offset'));
+      },
+      set_breakpoint: (id) => {
+        //console.log("set_breakpoint");
+        const { el } = module_state[id];
+        module_state[id]['breakpoint'] = el.getAttribute('data-carousel-breakpoint');
       },
       set_offset_left: (id) => {
         //console.log("set_offset_left");
@@ -100,29 +112,30 @@ storm_eagle.module('carousel', () => {
       },
       set_indicators: (id) => {
         //console.log("set_indicators");
-        const { el, indicators_group, total_children, number_of_active, current_active_carousel_item } = module_state[id];
+        const { el, items, indicators_group, number_of_active, current_active_carousel_item } = module_state[id];
         indicators_group.innerHTML = '';
-        for (let i = 0; i <= total_children - number_of_active; i++) {
+        for (let i = 0; i <= items.length - number_of_active; i++) {
           indicators_group.innerHTML += `<button name="carousel-control-button" data-carousel-indicator-control class="cursor:pointer"><span class="show-for-sr">Go to slide #${i + 1}</button>`;
         }
         module_state[id]['indicators_group_controls'] = el.querySelectorAll('[data-module="carousel.indicators-group"] > [data-carousel-indicator-control]');
         module_state[id]['indicators_group_controls'][current_active_carousel_item].setAttribute("data-carousel-indicator-active","true");
       },
-      set_carousel_item_width: (id) => {
-        // console.log("set_carousel_item_width");
-        const { el, item_group, items, total_children, offset_left, number_of_active } = module_state[id];
-        const carousel_item_width = (el.offsetWidth - 2 * offset_left) / number_of_active;
-        module_state[id]['carousel_item_width'] = carousel_item_width;
+      set_item_width: (id) => {
+        // console.log("set_item_width");
+        const { el, item_group, items, number_of_active, offset_left } = module_state[id];
+        const item_width = (el.offsetWidth - 2 * offset_left) / number_of_active;
+        module_state[id]['item_width'] = item_width;
         items.forEach((item) => {
-          item.style.width = `${carousel_item_width}px`;
+          item.style.width = `${item_width}px`;
         });
-        item_group.style.width = `${offset_left + carousel_item_width * total_children}px`;
+        item_group.style.width = `${offset_left + item_width * items.length}px`;
       },
     },
     ui: {
       is_active: (id) => {
         //console.log("is_active");
-        const { breakpoint, indicators_group, controls_prev, controls_next } = module_state[id];
+        self.state.set_breakpoint(id);
+        const { breakpoint } = module_state[id];
         switch (breakpoint) {
           case 'sm=':
             return storm_eagle.client.viewport.is_sm_only();
@@ -148,7 +161,7 @@ storm_eagle.module('carousel', () => {
       },
       update: (id) => {
         if (self.ui.is_active(id)) {
-          self.state.initialize(id);
+          self.state.update(id);
           self.ui.activate(id);
         } else {
           self.ui.disable(id);
@@ -159,25 +172,25 @@ storm_eagle.module('carousel', () => {
         self.ui.active_items.enable(id);
         self.ui.indicators_group.enable(id);
         self.ui.controls.enable(id);
-        self.ui.focusable_elements.enable(id);
         self.ui.item_group_position.enable(id);
         self.ui.transition.enable(id);
+        self.a11y.focusable_elements.enable(id);
       },
       disable: (id) => {
         self.ui.active_items.disable(id);
         self.ui.indicators_group.disable(id);
         self.ui.controls.disable(id);
-        self.ui.focusable_elements.disable(id);
         self.ui.item_group_position.disable(id);
+        self.a11y.focusable_elements.disable(id);
       },
       active_items: {
         enable: (id) => {
-          const { el, item_group, items, total_children, indicators_group_controls, number_of_active, offset_left, carousel_item_width, current_active_carousel_item, transition_duration } = module_state[id];
+          const { el, item_group, items, indicators_group_controls, number_of_active, offset_left, item_width, current_active_carousel_item, transition_duration } = module_state[id];
           el.setAttribute("data-carousel-active","true");
           /* resets the active classes on the carousel items and adds the proper active classes */
           items.forEach((item) => {
             item.setAttribute("data-carousel-item-active","");
-            item.setAttribute("data-carousel-item-secondary-active","");
+            item.setAttribute("data-carousel-item-secondary-active","false");
           });
           items[current_active_carousel_item].setAttribute("data-carousel-item-active","true");
           items[current_active_carousel_item].setAttribute("data-carousel-item-secondary-active","true");
@@ -203,7 +216,7 @@ storm_eagle.module('carousel', () => {
       },
       controls: {
         enable: (id) => {
-          const { controls_next, controls_prev, control_active_classes, total_children, control_inactive_classes, control_class_queryselector, breakpoint, current_active_carousel_item, number_of_active } = module_state[id];
+          const { items, controls_next, controls_prev, control_active_classes, control_inactive_classes, control_class_queryselector, breakpoint, current_active_carousel_item, number_of_active } = module_state[id];
 
           let temp_controls_prev, temp_controls_next;
           if (control_class_queryselector) {
@@ -215,7 +228,7 @@ storm_eagle.module('carousel', () => {
           }
 
           /* show both chevrons */
-          if (total_children !== 1) {
+          if (items.length !== 1) {
             controls_prev.removeAttribute("disabled");
             controls_next.removeAttribute("disabled");
             temp_controls_prev.classList.remove(...control_inactive_classes);
@@ -228,7 +241,7 @@ storm_eagle.module('carousel', () => {
             controls_prev.setAttribute("disabled","");
             temp_controls_prev.classList.add(...control_inactive_classes);
             temp_controls_prev.classList.remove(...control_active_classes);
-          } else if (current_active_carousel_item === total_children - number_of_active) {
+          } else if (current_active_carousel_item === items.length - number_of_active) {
             controls_next.setAttribute("disabled","");
             temp_controls_next.classList.add(...control_inactive_classes);
             temp_controls_next.classList.remove(...control_active_classes);
@@ -237,38 +250,12 @@ storm_eagle.module('carousel', () => {
         disable: (id) => {
         },
       },
-      focusable_elements: {
-        enable: (id) => {
-          const { el, items, focusable_element } = module_state[id];
-          /* ensures only links in the active carousel or tab-able */
-          items.forEach((item) => {
-            item.querySelectorAll(focus_trap_selector).forEach((focusable_element) => {
-              focusable_element.setAttribute('tabindex', '-1');
-              //focusable_element.setAttribute('disabled',"true");
-            });
-          });
-          el.querySelectorAll('[data-carousel-item-secondary-active="true"]').forEach((item) => {
-            item.querySelectorAll(focus_trap_selector).forEach((focusable_element) => {
-              focusable_element.setAttribute('tabindex', '0');
-              focusable_element.removeAttribute('disabled');
-            });
-          });
-        },
-        disable: (id) => {
-          const { items, focusable_element } = module_state[id];
-          items.forEach((item) => {
-            item.querySelectorAll(focus_trap_selector).forEach((focusable_element) => {
-              focusable_element.setAttribute('tabindex', '0');
-              focusable_element.removeAttribute('disabled');
-            });
-          });
-        },
-      },
+
       item_group_position: {
         enable: (id) => {
-          const { item_group, offset_left, current_active_carousel_item, carousel_item_width } = module_state[id];
+          const { item_group, item_width, offset_left, current_active_carousel_item } = module_state[id];
           /* changes the left offset */
-          item_group.style.left = `${offset_left - current_active_carousel_item * carousel_item_width}px`;
+          item_group.style.left = `${offset_left - current_active_carousel_item * item_width}px`;
         },
         disable: (id) => {
           const { item_group, items } = module_state[id];
@@ -319,7 +306,7 @@ storm_eagle.module('carousel', () => {
       control_buttons: {
         initialize: (id) => {
           //console.log("control_buttons initialize");
-          const { el, controls_next, controls_prev } = module_state[id];
+          const { controls_prev, controls_next } = module_state[id];
           controls_next.removeEventListener('click', self.event_listeners.control_buttons.handle_swipe_left);
           controls_next.addEventListener('click', self.event_listeners.control_buttons.handle_swipe_left);
           controls_prev.removeEventListener('click', self.event_listeners.control_buttons.handle_swipe_right);
@@ -329,14 +316,14 @@ storm_eagle.module('carousel', () => {
           //console.log("handle_swipe_left");
           event.preventDefault();
           const id = storm_eagle.util.closest_parent(event.currentTarget, '[data-module="carousel"]').getAttribute('id');
-          const { el, item_group } = module_state[id];
+          const { item_group } = module_state[id];
           item_group.dispatchEvent(new Event('swiped-left'));
         },
         handle_swipe_right: (event) => {
           //console.log("handle_swipe_right");
           event.preventDefault();
           const id = storm_eagle.util.closest_parent(event.currentTarget, '[data-module="carousel"]').getAttribute('id');
-          const { el, item_group } = module_state[id];
+          const { item_group } = module_state[id];
           item_group.dispatchEvent(new Event('swiped-right'));
         },
       },
@@ -353,7 +340,7 @@ storm_eagle.module('carousel', () => {
       swipe: {
         initialize: (id) => {
           //console.log("swipe initialize");
-          const { el, item_group } = module_state[id];
+          const { item_group } = module_state[id];
           item_group.removeEventListener('swiped-left', self.event_listeners.swipe.handle_swiped_left);
           item_group.addEventListener('swiped-left', self.event_listeners.swipe.handle_swiped_left);
           item_group.removeEventListener('swiped-right', self.event_listeners.swipe.handle_swiped_right);
@@ -362,9 +349,9 @@ storm_eagle.module('carousel', () => {
         handle_swiped_left: (event) => {
           //console.log('go -> in the carousel');
           const id = storm_eagle.util.closest_parent(event.currentTarget, '[data-module="carousel"]').getAttribute('id');
-          const { el, item_group, total_children, transition_duration, number_of_active, current_active_carousel_item } = module_state[id];
+          const { item_group, items, transition_duration, number_of_active, current_active_carousel_item } = module_state[id];
           item_group.style.transitionDuration = `${transition_duration}s`;
-          if (module_state[id]['current_active_carousel_item'] !== total_children - number_of_active) {
+          if (module_state[id]['current_active_carousel_item'] !== items.length - number_of_active) {
             module_state[id]['current_active_carousel_item']++;
             self.ui.update(id);
           }
@@ -372,7 +359,7 @@ storm_eagle.module('carousel', () => {
         handle_swiped_right: (event) => {
           //console.log('go <- in the carousel');
           const id = storm_eagle.util.closest_parent(event.currentTarget, '[data-module="carousel"]').getAttribute('id');
-          const { el, item_group, transition_duration } = module_state[id];
+          const { item_group, transition_duration } = module_state[id];
           item_group.style.transitionDuration = `${transition_duration}s`;
           if (module_state[id]['current_active_carousel_item'] !== 0) {
             module_state[id]['current_active_carousel_item']--;
@@ -381,6 +368,44 @@ storm_eagle.module('carousel', () => {
         },
       },
     },
-
+    a11y: {
+      focusable_elements: {
+        enable: (id) => {
+          const { el } = module_state[id];
+          /* ensures all elements in the carousel item with [data-carousel-item-secondary-active="false"] (partially visible) are not focusable */
+          el.querySelectorAll('[data-carousel-item-secondary-active="false"]').forEach((item) => {
+            item.querySelectorAll(focus_trap_selector).forEach((focusable_element) => {
+              focusable_element.setAttribute('tabindex', '-1');
+              focusable_element.setAttribute('disabled',"true");
+            });
+          });
+          /* resets the elements for carousel item with [data-carousel-item-secondary-active="true"] */
+          el.querySelectorAll('[data-carousel-item-secondary-active="true"]').forEach((item) => {
+            item.querySelectorAll("*").forEach((remove_disabled) => {
+              (remove_disabled.getAttribute('tabindex') === '-1') && remove_disabled.setAttribute('tabindex', '0');
+              remove_disabled.removeAttribute('disabled');
+            });
+          });
+          /* removes and unwanted focus selectors (if applicable, usually there won't be */
+          el.querySelectorAll('[data-carousel-item-secondary-active="true"]').forEach((item) => {
+            item.querySelectorAll(remove_focus_selector).forEach((remove_focusable_element) => {
+              remove_focusable_element.setAttribute('tabindex', '0');
+              remove_focusable_element.removeAttribute('disabled');
+            });
+          });
+          /* reinitialize the focusable elements for the dialogs */
+          (storm_eagle.dialog) && storm_eagle.dialog.initialize();
+        },
+        disable: (id) => {
+          const { items } = module_state[id];
+          items.forEach((item) => {
+            item.querySelectorAll(remove_focus_selector).forEach((remove_focusable_element) => {
+              remove_focusable_element.setAttribute('tabindex', '0');
+              remove_focusable_element.removeAttribute('disabled');
+            });
+          });
+        },
+      },
+    },
   };
 });
