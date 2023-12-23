76facfa6d17d12b812d260b6eb9c5b8bcba87cba
Warren Shea <warren.shea@gmail.com>
2023-12-23 00:28:50 -0500

    Major refactor

--- _dev/scripts/module.accordion.js
+++ _dev/scripts/module.accordion.js
@@ -5,15 +5,14 @@
 */
 storm_eagle.module('accordion', () => {
   let self;
-  let module_state = {};
-
+  let state = {};
   return {
     initialize: () => {
       self = storm_eagle.accordion;
-      module_state = {};
+      state = {};
       document.querySelectorAll('[data-module="accordion"]').forEach((el) => {
         const id = el.getAttribute('id');
-        module_state[id] = {
+        state[id] = {
           el,
           all_headers: el.querySelectorAll(':scope > div > * > [data-module="accordion.header"],:scope > [data-module="accordion.header"]'),
           all_panels: el.querySelectorAll(':scope > div > [data-module="accordion.panel"],:scope > [data-module="accordion.panel"]'),
@@ -22,11 +21,12 @@ storm_eagle.module('accordion', () => {
         };
         self.ui.initialize(id);
         self.event_listeners.initialize(id);
+        self.ui.initialize_initial_active(id);
       });
     },
     ui: {
       initialize: (id) => {
-        const { el, all_headers, all_panels } = module_state[id];
+        const { el, all_headers, all_panels } = state[id];
         all_headers.forEach((header, index) => {
           const panel = header.parentNode.nextElementSibling;
           header.setAttribute('tabindex', '-1');
@@ -35,12 +35,10 @@ storm_eagle.module('accordion', () => {
           panel.setAttribute('aria-labelledby', header.getAttribute('id'));
           if (index === 0) {
             header.setAttribute('tabindex', '0');
-            header.addEventListener('focusin', () => {
-              el.classList.add('focus');
-            });
-            header.addEventListener('focusout', () => {
-              el.classList.remove('focus');
-            });
+            header.removeEventListener('focusin', self.event_listeners.header_focus.add);
+            header.addEventListener('focusin', self.event_listeners.header_focus.add);
+            header.removeEventListener('focusout', self.event_listeners.header_focus.remove);
+            header.addEventListener('focusout', self.event_listeners.header_focus.remove);
           }
         });
         all_panels.forEach((panel) => {
@@ -48,25 +46,26 @@ storm_eagle.module('accordion', () => {
         });
       },
       initialize_initial_active: (id) => {
-        const { all_headers, initial_active } = module_state[id];
+        const { all_headers, initial_active } = state[id];
         all_headers.forEach((header,index) => {
-          (initial_active[index] === 1) && header.click();
+          if (initial_active[index] === 1) {
+            header.click();
+          }
         });
       },
     },
     event_listeners: {
       initialize: (id) => {
-        const { all_headers } = module_state[id];
+        const { all_headers } = state[id];
         all_headers.forEach((header) => {
           header.removeEventListener('click', self.event_listeners.update_accordion_item);
           header.addEventListener('click', self.event_listeners.update_accordion_item);
         });
-        self.ui.initialize_initial_active(id);
       },
       update_accordion_item: (event) => {
         const el = event.currentTarget;
         const module_id = storm_eagle.util.closest_parent(el,`[data-module='accordion']`).getAttribute("id");
-        const { all_headers, active_setting } = module_state[module_id];
+        const { all_headers, active_setting } = state[module_id];
         if (active_setting === 'single') {
           all_headers.forEach((header) => {
             header.setAttribute('aria-expanded', 'false');
@@ -78,9 +77,17 @@ storm_eagle.module('accordion', () => {
           self.open(el.getAttribute('aria-controls'),module_id);
         }
       },
+      header_focus: {
+        add: (event) => {
+          event.currentTarget.classList.add('focus');
+        },
+        remove: (event) => {
+          event.currentTarget.classList.remove('focus');
+        },
+      }
     },
     open: (panel_id, module_id) => {
-      const { active_setting, all_panels } = module_state[module_id];
+      const { active_setting, all_panels } = state[module_id];
       if (active_setting === 'single') {
         all_panels.forEach((panel) => {
           panel.setAttribute("data-accordion-panel","hide");
@@ -89,8 +96,7 @@ storm_eagle.module('accordion', () => {
         storm_eagle.equalize_heights.force_resize();
       } else if (active_setting === 'multiple') {
         const panel = document.getElementById(panel_id);
-        const current_state = panel.getAttribute("data-accordion-panel");
-        panel.setAttribute("data-accordion-panel", current_state === "hide" ? "" : "hide");
+        panel.setAttribute("data-accordion-panel", panel.getAttribute("data-accordion-panel") === "hide" ? "" : "hide");
         storm_eagle.equalize_heights.force_resize();
       }
     },
