7ab7a9468d7c60c0c03d5ae5966beb8bd6c010f9
Warren Shea <warren.shea@gmail.com>
2023-12-26 21:27:28 -0500

    Major refactor

--- _dev/scripts/module.menu-submenu.js
+++ _dev/scripts/module.menu-submenu.js
@@ -5,13 +5,14 @@
 */
 storm_eagle.module('menu_submenu', () => {
   let self;
-  let menu_state = {};
+  let state = {};
   return {
     initialize: () => {
       self = storm_eagle.menu_submenu;
       document.querySelectorAll('[data-module="menu"]').forEach((el) => {
-        let menu_id = el.getAttribute('id');
-        menu_state[menu_id] = {
+        const id = el.getAttribute('id');
+        state[id] = {
+          el,
           all_triggers: el.querySelectorAll(':scope > li > [data-module="menu.trigger"],:scope > [data-module="menu.trigger"]'),
           all_submenus: el.querySelectorAll(':scope > li > [data-module="menu.submenu"],:scope > [data-module="menu.submenu"]'),
           active_setting: el.getAttribute('data-menu-active'),
@@ -19,70 +20,91 @@ storm_eagle.module('menu_submenu', () => {
           inactive_classes: el.getAttribute('data-menu-inactive-classes') ? el.getAttribute('data-menu-inactive-classes').split(',') : '',
           open_on_load: el.querySelectorAll(':scope > li > [data-module="menu.trigger"][data-menu-open],:scope > [data-module="menu.trigger"][data-menu-open]'),
         };
-        self.init_ui(menu_id);
-        self.add_event_listeners(menu_id);
-        menu_state[menu_id]['open_on_load'].forEach((el) => {
-          el.click();
-        });
+        self.ui.initialize(id);
+        self.event_listeners.initialize(id);
+        self.ui.initialize_initial_active(id);
       });
     },
-    init_ui: (menu_id) => {
-      menu_state[menu_id]['all_triggers'].forEach((el, index) => {
-        el.setAttribute('tabindex', '-1');
-        el.setAttribute('aria-expanded', 'false');
-        el.setAttribute('aria-controls', el.nextElementSibling.getAttribute('id'));
-        el.classList.add(...menu_state[menu_id]['inactive_classes']);
-        if (index === 0) {
-          el.setAttribute('tabindex', '0');
-          el.addEventListener('focusin', () => {
-            document.getElementById(menu_id).classList.add('focus');
-          });
-          el.addEventListener('focusout', () => {
-            document.getElementById(menu_id).classList.remove('focus');
-          });
-        }
-      });
-      menu_state[menu_id]['all_submenus'].forEach((el) => {
-        el.classList.add('display:none');
-      });
-    },
-    add_event_listeners: (menu_id) => {
-      menu_state[menu_id]['all_triggers'].forEach((el) => {
-        el.addEventListener('click', () => {
-          let aria_expanded_initial_state = el.getAttribute('aria-expanded');
-          if (menu_state[menu_id]['active_setting'] === 'single') {
-            menu_state[menu_id]['all_triggers'].forEach((el) => {
-              el.classList.remove(...menu_state[menu_id]['active_classes']);
-              el.classList.add(...menu_state[menu_id]['inactive_classes']);
-              el.setAttribute('aria-expanded', 'false');
-            });
-            el.setAttribute('aria-expanded', 'true');
-            el.classList.add(...menu_state[menu_id]['active_classes']);
-            el.classList.remove(...menu_state[menu_id]['inactive_classes']);
-          } else if (menu_state[menu_id]['active_setting'] === 'multiple') {
-            if (aria_expanded_initial_state === 'false') {
-              el.setAttribute('aria-expanded', 'true');
-              el.classList.add(...menu_state[menu_id]['active_classes']);
-              el.classList.remove(...menu_state[menu_id]['inactive_classes']);
-            } else {
-              el.setAttribute('aria-expanded', 'false');
-              el.classList.remove(...menu_state[menu_id]['active_classes']);
-              el.classList.add(...menu_state[menu_id]['inactive_classes']);
-            }
+    ui: {
+      initialize: (id) => {
+        const { el, all_triggers, all_submenus, inactive_classes } = state[id];
+        all_triggers.forEach((el, index) => {
+          el.setAttribute('tabindex', '-1');
+          el.setAttribute('aria-expanded', 'false');
+          el.setAttribute('aria-controls', el.nextElementSibling.getAttribute('id'));
+          el.classList.add(...inactive_classes);
+          if (index === 0) {
+            el.setAttribute('tabindex', '0');
+            el.removeEventListener('focusin', self.event_listeners.menu_focus.add);
+            el.addEventListener('focusin', self.event_listeners.menu_focus.add);
+            el.removeEventListener('focusout', self.event_listeners.menu_focus.remove);
+            el.addEventListener('focusout', self.event_listeners.menu_focus.remove);
           }
-          self.open(menu_id, el.getAttribute('aria-controls'));
         });
-      });
-    },
-    open: (menu_id, id) => {
-      if (menu_state[menu_id]['active_setting'] === 'single') {
-        menu_state[menu_id]['all_submenus'].forEach((el) => {
+        all_submenus.forEach((el) => {
           el.classList.add('display:none');
         });
-        document.getElementById(id).classList.remove('display:none');
+      },
+      initialize_initial_active: (id) => {
+        const { el, open_on_load } = state[id];
+        open_on_load.forEach((el) => {
+          el.click();
+        });
+      },
+    },
+    event_listeners: {
+      initialize: (id) => {
+        const { el, all_triggers } = state[id];
+        all_triggers.forEach((el) => {
+          el.removeEventListener('click', self.event_listeners.trigger_click);
+          el.addEventListener('click', self.event_listeners.trigger_click);
+        });
+      },
+      menu_focus: {
+        add: (event) => {
+          event.currentTarget.classList.add('focus');
+        },
+        remove: (event) => {
+          event.currentTarget.classList.remove('focus');
+        },
+      },
+      trigger_click: (event) => {
+        const el = event.currentTarget;
+        const id = storm_eagle.util.closest_parent(el,`[data-module='menu']`).getAttribute("id");
+        const { all_triggers, active_setting, active_classes, inactive_classes } = state[id];
+        if (active_setting === 'single') {
+          all_triggers.forEach((el) => {
+            el.classList.remove(...active_classes);
+            el.classList.add(...inactive_classes);
+            el.setAttribute('aria-expanded', 'false');
+          });
+          el.setAttribute('aria-expanded', 'true');
+          el.classList.add(...active_classes);
+          el.classList.remove(...inactive_classes);
+        } else if (active_setting === 'multiple') {
+          if (el.getAttribute('aria-expanded') === 'false') {
+            el.setAttribute('aria-expanded', 'true');
+            el.classList.add(...active_classes);
+            el.classList.remove(...inactive_classes);
+          } else {
+            el.setAttribute('aria-expanded', 'false');
+            el.classList.remove(...active_classes);
+            el.classList.add(...inactive_classes);
+          }
+        }
+        self.open(id, el.getAttribute('aria-controls'));
+      }
+    },
+    open: (id, menu_panel_id) => {
+      const { el, all_submenus, active_setting } = state[id];
+      if (active_setting === 'single') {
+        all_submenus.forEach((el) => {
+          el.classList.add('display:none');
+        });
+        document.getElementById(menu_panel_id).classList.remove('display:none');
         storm_eagle.equalize_heights.force_resize();
-      } else if (menu_state[menu_id]['active_setting'] === 'multiple') {
-        document.getElementById(id).classList.toggle('display:none');
+      } else if (active_setting === 'multiple') {
+        document.getElementById(menu_panel_id).classList.toggle('display:none');
         storm_eagle.equalize_heights.force_resize();
       }
     },
