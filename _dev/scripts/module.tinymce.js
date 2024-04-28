ed2f3923b148c377826fb4254f66abfab6062c05
Warren Shea <warren.shea@gmail.com>
2024-04-27 21:41:40 -0400

    Minor

--- _dev/scripts/module.tinymce.js
+++ _dev/scripts/module.tinymce.js
@@ -21,7 +21,7 @@ storm_eagle.module('tinymce', () => {
   };
 
   const tinymce_basic = {
-    height: 125,
+    height: 250,
     forced_root_block: 'faketag', //prevents a wrapping <p> tag as these should have no wrapper
     plugins: 'anchor charmap code codesample emoticons fullscreen help image insertdatetime link lists media nonbreaking pagebreak preview save searchreplace table visualblocks visualchars wordcount',
     toolbar: "undo redo | italic superscript | link openlink unlink | charmap | visualblocks | advanced",
