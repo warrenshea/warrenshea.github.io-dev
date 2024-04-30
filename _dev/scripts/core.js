7f7d30a13a96276d4bbee916bfb1c5f915cb651b
Warren Shea <warren.shea@gmail.com>
2024-04-30 12:45:45 -0400

    Minor

--- _dev/scripts/core.js
+++ _dev/scripts/core.js
@@ -1248,14 +1248,14 @@ storm_eagle.module('zs_highlighter', () => {
     initialize: () => {
       if (storm_eagle.page.get_query_value('zs-highlight-mid')) {
         storm_eagle.page.get_query_value('zs-highlight-mid').split(",").forEach((mid_id) => {
-          document.querySelectorAll(`[data-zs*=mid\\:${mid_id}]`).forEach((element) => {
+          document.querySelectorAll(`[data-zs*=mid\\:'${mid_id},']`).forEach((element) => {
             element.classList.add("b-yellow:4px","p:4px");
           })
         });
       }
       if (storm_eagle.page.get_query_value('zs-highlight-cid')) {
         storm_eagle.page.get_query_value('zs-highlight-cid').split(",").forEach((cid_id) => {
-          document.querySelectorAll(`[data-zs*=cid\\:${cid_id}]`).forEach((element) => {
+          document.querySelectorAll(`[data-zs*=cid\\:'${cid_id},']`).forEach((element) => {
             element.classList.add("b-red:4px","p:4px");
           })
         });
