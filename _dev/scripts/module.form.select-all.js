27baa307b51b11f17f50de1b81a236b5d47ca2a6
Warren Shea <warren.shea@gmail.com>
2024-07-15 23:08:58 -0400

    Minor refactor

--- _dev/scripts/module.form.select-all.js
+++ _dev/scripts/module.form.select-all.js
@@ -21,10 +21,11 @@ storm_eagle.module('form.select_all', () => {
           el,
           type: el.getAttribute('data-select-all-type'),
           type_elements: (el.getAttribute('data-select-all-type') === "checkbox") ? el.querySelectorAll("input[type='checkbox']") : el.querySelectorAll("button[type='button']") ,
-          select_all_some_none: el.getAttribute('data-select-all-bind'),
+          select_all_radiobuttons_id: el.getAttribute('data-select-all-bind'),
           onupdate: el.getAttribute('data-select-all-onupdate-func') || false,
         };
-        self.event_listeners.initialize(id);
+        self.event_listeners.elements.initialize(id);
+        self.event_listeners.select_all_radiobuttons.initialize(id);
       });
     },
     ui: {
@@ -33,7 +34,7 @@ storm_eagle.module('form.select_all', () => {
       },
       set_select_all_radio: (id) => {
         let count = 0;
-        const { type, type_elements, select_all_some_none, onupdate } = state[id];
+        const { type, type_elements, select_all_radiobuttons_id, onupdate } = state[id];
         type_elements.forEach((el) => {
           if (type === 'checkbox') {
             if (storm_eagle.checkbox.is_checked(el)) {
@@ -47,20 +48,20 @@ storm_eagle.module('form.select_all', () => {
         });
         if (count === 0) {
           /* None */
-          document.querySelectorAll(`#${select_all_some_none} > input`)[1].checked = true;
+          document.querySelectorAll(`#${select_all_radiobuttons_id} > input`)[1].checked = true;
         } else if (count === type_elements.length) {
           /* All */
-          document.querySelectorAll(`#${select_all_some_none} > input`)[0].checked = true;
+          document.querySelectorAll(`#${select_all_radiobuttons_id} > input`)[0].checked = true;
         } else {
           /* Some */
-          document.querySelectorAll(`#${select_all_some_none} > input`)[2].checked = true;
+          document.querySelectorAll(`#${select_all_radiobuttons_id} > input`)[2].checked = true;
         }
         if (onupdate) {
           storm_eagle.util.run_str_func( onupdate, { id } );
         }
       },
       set_all_elements: (id, value) => {
-        const { type, type_elements, select_all_some_none, onupdate } = state[id];
+        const { type, type_elements, select_all_radiobuttons_id, onupdate } = state[id];
         type_elements.forEach((el) => {
           if (el.getAttribute("data-module") === "parent-checkbox") {
             storm_eagle.checkbox.set_aria_checked(el, value);
@@ -79,15 +80,28 @@ storm_eagle.module('form.select_all', () => {
       },
     },
     event_listeners: {
-      initialize: (id) => {
-        const { type_elements } = state[id];
-        type_elements.forEach((el) => {
-          el.addEventListener('click', (event) => {
-            setTimeout(() => {
-              self.ui.set_select_all_radio(id);
-            }, 0);
+      elements: {
+        initialize: (id) => {
+          const { type_elements } = state[id];
+          type_elements.forEach((el) => {
+            el.addEventListener('click', (event) => {
+              setTimeout(() => {
+                self.ui.set_select_all_radio(id);
+              }, 0);
+            });
           });
-        });
+        }
+      },
+      select_all_radiobuttons: {
+        initialize: (id) => {
+          const { select_all_radiobuttons_id } = state[id];
+          document.querySelectorAll(`#${select_all_radiobuttons_id} > input`)[0].addEventListener('click', (event) => {
+            self.ui.set_all_elements(id, true);
+          });
+          document.querySelectorAll(`#${select_all_radiobuttons_id} > input`)[1].addEventListener('click', (event) => {
+            self.ui.set_all_elements(id, false);
+          });
+        }
       },
     },
   };
