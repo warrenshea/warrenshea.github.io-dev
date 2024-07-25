20ea71841c1efe8fff71fe73eb8e56f3b332bf32
Warren Shea <warren.shea@gmail.com>
2024-07-25 08:02:24 -0400

    Major refactor for event listeners and other minor fixes

--- _dev/scripts/module.isotope.js
+++ _dev/scripts/module.isotope.js
@@ -68,30 +68,49 @@ storm_eagle.module('isotope', () => {
           sort_by: (id) => {
             const { sort_by_id } = state[id];
             if (sort_by_id) {
-              state[id].sort_by_value = storm_eagle.radiobutton.get_value(`#${sort_by_id} > input`);
+              state[id].sort_by_value = storm_eagle.radiobutton.get_value(`#${sort_by_id} > input`, 'data-isotope-sort-value');
             }
           },
           sort_ascending: (id) => {
             const { sort_ascending_id } = state[id];
             if (sort_ascending_id) {
-              state[id].sort_ascending_value = JSON.parse(storm_eagle.radiobutton.get_value(`#${sort_ascending_id} > input`));
+              state[id].sort_ascending_value = JSON.parse(storm_eagle.radiobutton.get_value(`#${sort_ascending_id} > input`, 'data-isotope-sort-value'));
             }
           },
           filter: (id) => {
             const { filter_initial, filter_no_results, filter_groups_ids, filter_groups_types } = state[id];
+            let filter_group_elem;
             if (filter_groups_ids.length > 0) {
               filter_groups_ids.forEach((filter_group_id, index) => {
                 switch (filter_groups_types[index]) {
                   case "checkbox":
+                    // const all_checkboxes = document.querySelectorAll(`#${filter_group_id} > input`);
+                    // if (Array.from(all_checkboxes).every(checkbox => checkbox.checked)) {
+                    //   state[id].filters[filter_group_id] = '*';
+                    // } else {
+                    //   let filter_group_checkbox_values = storm_eagle.checkbox.get_values(`#${filter_group_id} > input`,'data-isotope-filter-value');
+
+                    //   if (document.querySelector(`#${filter_group_id}`).hasAttribute("data-isotope-or-and-bind")) {
+                    //     const or_and_value = storm_eagle.radiobutton.get_value(`#${document.querySelector(`#${filter_group_id}`).getAttribute("data-isotope-or-and-bind")} > input`);
+                    //     filter_group_checkbox_values = (or_and_value === "and") ? [filter_group_checkbox_values.join('') || filter_no_results] : filter_group_checkbox_values;
+                    //   }
+                    //   state[id].filters[filter_group_id] = filter_group_checkbox_values;
+                    // }
+                    // break;
+
                     const all_checkboxes = document.querySelectorAll(`#${filter_group_id} > input`);
-                    if (Array.from(all_checkboxes).every(checkbox => checkbox.checked)) {
+                    const all_checkboxes_checked = Array.from(all_checkboxes).every(checkbox => checkbox.checked);
+                    filter_group_elem = document.querySelector(`#${filter_group_id}`);
+
+                    if (all_checkboxes_checked) {
                       state[id].filters[filter_group_id] = '*';
                     } else {
-                      let filter_group_checkbox_values = storm_eagle.checkbox.get_values(`#${filter_group_id} > input`,'data-isotope-filter-value');
+                      let filter_group_checkbox_values = storm_eagle.checkbox.get_values(`#${filter_group_id} > input`, 'data-isotope-filter-value');
 
-                      if (document.querySelector(`#${filter_group_id}`).hasAttribute("data-isotope-or-and-bind")) {
-                        const or_and_value = storm_eagle.radiobutton.get_value(`#${document.querySelector(`#${filter_group_id}`).getAttribute("data-isotope-or-and-bind")} > input`);
-                        filter_group_checkbox_values = (or_and_value === "and") ? [filter_group_checkbox_values.join('') || filter_no_results] : filter_group_checkbox_values;
+                      if (filter_group_elem.hasAttribute('data-isotope-or-and-bind')) {
+                        const or_and_bind_id = filter_group_elem.getAttribute('data-isotope-or-and-bind');
+                        const or_and_value = storm_eagle.radiobutton.get_value(`#${or_and_bind_id} > input`);
+                        filter_group_checkbox_values = (or_and_value === 'and') ? [filter_group_checkbox_values.join('') || filter_no_results] : filter_group_checkbox_values;
                       }
                       state[id].filters[filter_group_id] = filter_group_checkbox_values;
                     }
@@ -106,6 +125,25 @@ storm_eagle.module('isotope', () => {
                       } else {
                         state[id].filters[filter_group_id] = (JSON.parse(document.querySelector(`#${filter_group_id}`).getAttribute('data-autocomplete-values')).map(str => str.toLowerCase()));
                       }
+                    }
+                    break;
+                  case "switch":
+                    const all_switches = document.querySelectorAll(`#${filter_group_id} > button`);
+                    const all_switches_checked = Array.from(document.querySelectorAll('button[aria-checked]')).every(button => button.getAttribute('aria-checked') === 'true');
+
+                    filter_group_elem = document.querySelector(`#${filter_group_id}`);
+
+                    if (all_switches_checked) {
+                      state[id].filters[filter_group_id] = '*';
+                    } else {
+                      let filter_group_checkbox_values = storm_eagle.button.get_values(`#${filter_group_id} > button`, 'data-isotope-filter-value');
+
+                      if (filter_group_elem.hasAttribute('data-isotope-or-and-bind')) {
+                        const or_and_bind_id = filter_group_elem.getAttribute('data-isotope-or-and-bind');
+                        const or_and_value = storm_eagle.button.get_value(`#${or_and_bind_id} > button`);
+                        filter_group_checkbox_values = (or_and_value === 'and') ? [filter_group_checkbox_values.join('') || filter_no_results] : filter_group_checkbox_values;
+                      }
+                      state[id].filters[filter_group_id] = filter_group_checkbox_values;
                     }
                     break;
                   default:
@@ -171,14 +209,29 @@ storm_eagle.module('isotope', () => {
         self.event_listeners.select_all(id);
         self.event_listeners.or_and(id);
       },
+      set: {
+        sort_by: () => {
+          const id = storm_eagle.util.closest_parent(event.currentTarget, '[data-module="isotope"]').getAttribute('id');
+          self.config.state.set.sort_by(id);
+          self.ui.refresh(id);
+        },
+        sort_ascending: () => {
+          const id = storm_eagle.util.closest_parent(event.currentTarget, '[data-module="isotope"]').getAttribute('id');
+          self.config.state.set.sort_ascending(id);
+          self.ui.refresh(id);
+        },
+        filter: () => {
+          const id = storm_eagle.util.closest_parent(event.currentTarget, '[data-module="isotope"]').getAttribute('id');
+          self.config.state.set.filter(id);
+          self.ui.refresh(id);
+        },
+      },
       sort_by: (id) => {
         const { sort_by_id } = state[id];
         if (sort_by_id) {
           document.querySelectorAll(`#${sort_by_id} > input`).forEach((el) => {
-            el.addEventListener('click', () => {
-              self.config.state.set.sort_by(id);
-              self.ui.refresh(id);
-            });
+            el.removeEventListener('click', self.event_listeners.set.sort_by);
+            el.addEventListener('click', self.event_listeners.set.sort_by);
           });
         }
       },
@@ -186,37 +239,28 @@ storm_eagle.module('isotope', () => {
         const { sort_ascending_id } = state[id];
         if (sort_ascending_id) {
           document.querySelectorAll(`#${sort_ascending_id} > input`).forEach((el) => {
-            el.addEventListener('click', () => {
-              self.config.state.set.sort_ascending(id);
-              self.ui.refresh(id);
-            });
+            el.removeEventListener('click', self.event_listeners.set.sort_ascending);
+            el.addEventListener('click', self.event_listeners.set.sort_ascending);
           });
         }
       },
       filters: (id) => {
         const { filter_groups_ids } = state[id];
         filter_groups_ids.forEach((filter_group_id) => {
-            document.querySelectorAll(`#${filter_group_id} > input`).forEach((el) => {
-              el.addEventListener('click', () => {
-                self.config.state.set.filter(id);
-                self.ui.refresh(id);
-              });
-            });
-          //}
+          document.querySelectorAll(`#${filter_group_id} > input, #${filter_group_id} > button`).forEach((el) => {
+            el.removeEventListener('click', self.event_listeners.set.filter);
+            el.addEventListener('click', self.event_listeners.set.filter);
+          });
         });
       },
       select_all: (id) => {
         const { el } = state[id];
         el.querySelectorAll(`[data-module="select-all"]`).forEach((select_all_el) => {
           const select_all_radiobuttons_id = select_all_el.getAttribute('data-select-all-bind');
-          el.querySelector(`#${select_all_radiobuttons_id} > [data-module='select-all.all']`).addEventListener('click', () => {
-            self.config.state.set.filter(id);
-            self.ui.refresh(id);
-          });
-          el.querySelector(`#${select_all_radiobuttons_id} > [data-module='select-all.none']`).addEventListener('click', () => {
-            self.config.state.set.filter(id);
-            self.ui.refresh(id);
-          });
+          el.querySelector(`#${select_all_radiobuttons_id} > [data-module='select-all.all']`).removeEventListener('click', self.event_listeners.set.filter);
+          el.querySelector(`#${select_all_radiobuttons_id} > [data-module='select-all.all']`).addEventListener('click', self.event_listeners.set.filter);
+          el.querySelector(`#${select_all_radiobuttons_id} > [data-module='select-all.none']`).removeEventListener('click', self.event_listeners.set.filter);
+          el.querySelector(`#${select_all_radiobuttons_id} > [data-module='select-all.none']`).addEventListener('click', self.event_listeners.set.filter);
         });
       },
       or_and: (id) => {
@@ -224,17 +268,14 @@ storm_eagle.module('isotope', () => {
         el.querySelectorAll(`[data-module="select-all"]`).forEach((select_all_el) => {
           if (select_all_el.getAttribute('data-isotope-or-and-bind')) {
             const isotope_or_and_radiobuttons_id = select_all_el.getAttribute('data-isotope-or-and-bind');
-            el.querySelector(`#${isotope_or_and_radiobuttons_id} > [data-module='isotope.or']`).addEventListener('click', () => {
-              self.config.state.set.filter(id);
-              self.ui.refresh(id);
-            });
-            el.querySelector(`#${isotope_or_and_radiobuttons_id} > [data-module='isotope.and']`).addEventListener('click', () => {
-              self.config.state.set.filter(id);
-              self.ui.refresh(id);
-            });
+            el.querySelector(`#${isotope_or_and_radiobuttons_id} > [data-module='isotope.or']`).removeEventListener('click', self.event_listeners.set.filter);
+            el.querySelector(`#${isotope_or_and_radiobuttons_id} > [data-module='isotope.or']`).addEventListener('click', self.event_listeners.set.filter);
+            el.querySelector(`#${isotope_or_and_radiobuttons_id} > [data-module='isotope.and']`).removeEventListener('click', self.event_listeners.set.filter);
+            el.querySelector(`#${isotope_or_and_radiobuttons_id} > [data-module='isotope.and']`).addEventListener('click', self.event_listeners.set.filter);
           }
         });
       }
+
     },
     util: {
       combine_obj_value: (obj) => {
